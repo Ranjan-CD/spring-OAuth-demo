05746f1cafba639aa4a65fecf8547dac923691da
Giri_Ranjan <69512051+Ranjan-CD@users.noreply.github.com>
2025-08-02 21:46:38 +0530

    Spring-OAuth-demo
    Spring-OAuth-demo

---
+++ springOAuthdemo/springOAuthdemo/src/main/java/com/example/springOAuthdemo/SpringOAuthdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springOAuthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuthdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuthdemoApplication.class, args);
+	}
+
+}
---
+++ springOAuthdemo/springOAuthdemo/src/test/java/com/example/springOAuthdemo/SpringOAuthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springOAuthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
